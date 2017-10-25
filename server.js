dd108c995981b7153b8f585ffda17089bbcbdb39
Vishwanath AV <vishwanathavin@gmail.com>
2017-10-25 16:42:53 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -7,20 +7,20 @@ app.use(morgan('combined'));
 
 var articles = {
     'article-one': {
-	title : 'Vishwa article-one',
-	heading : 'article-one',
-	date : 'Oct 26th',
-	content : `Nothing
-	 nothing
-	 nothing`
+  title : 'Vishwa article-one',
+  heading : 'article-one',
+  date : 'Oct 26th',
+  content : `Nothing
+   nothing
+   nothing`
     },
     'article-two': {
-	title : 'Vishwa article-two',
-	heading : 'article-two',
-	date : 'Oct 27th',
-	content : `Nothing
-	 nothing
-	 nothing`
+  title : 'Vishwa article-two',
+  heading : 'article-two',
+  date : 'Oct 27th',
+  content : `Nothing
+   nothing
+   nothing`
     }
 };
 
@@ -38,7 +38,12 @@ function createData(data)
     </html>`;
     return htmltemplate;
 }
-
+var counter=0;
+app.get('/counter',function(req,res)
+{
+  counter = counter+1;
+  res.send(counter.toString());
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -54,6 +59,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
@@ -62,7 +71,7 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
+var port = 8080;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
