8e7fb9587af589550a42f6a4041e4f3b22dc295f
Vishwanath AV <vishwanathavin@gmail.com>
2017-10-28 21:13:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,25 @@
-console.log('Loaded!');
 var button = document.getElementById('counter');
 
 
 //create a request
 button.onclick = function(){
+
+	//create a request
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         if(request.readyState == XMLHttpRequest.DONE){
             //Take some action
             if(requst.status == 200){
-                var counter = requst.responseText();
+                var counter = request.responseText;
                 var span = document.getElementById('count');	
 	            span.innerHTML = counter.toString();
             }
         }
     };
-    request.open('GET', 'http://vishwanathavin.imad.hasura-app.io/counter',true);
+
+    //make a request
+    request.open('GET', 'localhost:8080/counter',true);
     request.send(null);
+
+	
 };
